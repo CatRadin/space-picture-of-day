6978afa10c1c074b076558c7be7e03fe388397ab
Catherine Radin <73500682+CatRadin@users.noreply.github.com>
2021-01-21 13:53:45 -0600

    made theme page

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { SRLWrapper } from "simple-react-lightbox";
 
 
 export default function Photo(props){
@@ -6,8 +7,8 @@ export default function Photo(props){
     
     
     return(
-        
+        <SRLWrapper>
         <img className='pic-of-day' src={nasaData.url} alt = 'space'></img>
-        
+        </SRLWrapper>
     )
 }
